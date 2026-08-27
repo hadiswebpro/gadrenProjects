b763eb94e13fc63d0643ced728c4df52c925da4e
Hadis <hadiswebpro@gmail.com>
2026-08-28 02:49:24 +0330

    color and readblr

--- src/modules/theme.js
+++ src/modules/theme.js
@@ -2,32 +2,32 @@ const themes = {
 
     burgundy: {
         light: "#ead8d5",
-        main: "#8f3d4a",
-        dark: "#54242c"
+        main: "#e32323",
+        dark: "#96021b"
     },
 
     sage: {
-        light: "#dfe7dc",
+        light: "#bed8b5",
         main: "#7b9470",
         dark: "#46583f"
     },
 
     olive: {
-        light: "#e5e4d2",
+        light: "#e5e2a8",
         main: "#92934f",
         dark: "#5c5d2f"
     },
 
     forest: {
-        light: "#d8e2dc",
+        light: "#bceace",
         main: "#3f6855",
         dark: "#284638"
     },
 
     rose: {
-        light: "#f0dddd",
-        main: "#b86f78",
-        dark: "#75434a"
+        light: "#f5bf81",
+        main: "#e6a42b",
+        dark: "#af7a2a"
     }
 
 };
