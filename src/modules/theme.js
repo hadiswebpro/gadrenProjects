43a47081c8e5a971603212647e903e3e0dca3132
Hadis <hadiswebpro@gmail.com>
2026-08-28 17:40:14 +0330

    Fix project themes and add violet theme

--- src/modules/theme.js
+++ src/modules/theme.js
@@ -1,47 +1,39 @@
 const themes = {
+  burgundy: {
+    light: "#ead8d5",
+    main: "#e32323",
+    dark: "#96021b",
+  },
 
-    burgundy: {
-        light: "#ead8d5",
-        main: "#e32323",
-        dark: "#96021b"
-    },
+  sage: {
+    light: "#bed8b5",
+    main: "#7b9470",
+    dark: "#46583f",
+  },
 
-    sage: {
-        light: "#bed8b5",
-        main: "#7b9470",
-        dark: "#46583f"
-    },
+  olive: {
+    light: "#e5e2a8",
+    main: "#92934f",
+    dark: "#5c5d2f",
+  },
 
-    olive: {
-        light: "#e5e2a8",
-        main: "#92934f",
-        dark: "#5c5d2f"
-    },
+  violet: {
+    light: "#ddd0f0",
+    main: "#7c4d9e",
+    dark: "#4f2868",
+  },
 
-    forest: {
-        light: "#bceace",
-        main: "#3f6855",
-        dark: "#284638"
-    },
-
-    rose: {
-        light: "#f5bf81",
-        main: "#e6a42b",
-        dark: "#af7a2a"
-    }
-
+  rose: {
+    light: "#f5bf81",
+    main: "#e6a42b",
+    dark: "#af7a2a",
+  },
 };
 
-
 export function getTheme(name) {
-
-    return themes[name];
-
+  return themes[name];
 }
 
-
 export function getThemes() {
-
-    return themes;
-
+  return themes;
 }
